7b6aa18408dfd3f0db3c9e844f307b8bd7f63a76
AFortin3 <antfortin.pro@gmail.com>
2026-06-04 15:23:08 +0200

    ajout bouton guide utilisateur

--- src/app.py
+++ src/app.py
@@ -3,20 +3,39 @@ import streamlit as st
 import core.GasSimulator as gsim
 from components.sidebar import render_sidebar
 from components.dashboard import show_gas_properties, show_limites_results, render_analysis_expander
+from components.guide import GUIDE_MD 
 
 def main():
     if "gsim" not in st.session_state:
         st.session_state.gsim = gsim.GasSimulator() 
             
-    st.set_page_config(layout="wide")
-        
-    st.title("Simulateur des Limites d'Inflammabilité")
-    
+    st.set_page_config(layout="wide") # pour prendre toute la largeur de l'écran
+
+    # On aligne le titre et le bouton pour afficher le guide utilisateur avec des colonnes
+    # col1 prend 80% de la largeur, col2 prend 20%
+    title, guide = st.columns([4, 1]) 
+
+    # On définit la fenêtre pop-up (guide utilisateur)
+    @st.dialog("📖 Guide d'Utilisation", width="large")
+    def afficher_guide():
+        st.markdown(GUIDE_MD)
+
+    # Titre de la page
+    with title:
+        st.title("Simulateur des Limites d'Inflammabilité")
+
+    with guide:
+        st.write("") # Petit espace vide pour aligner verticalement le bouton
+        if st.button("❓ Aide / Guide"):
+            afficher_guide() # Ouvre la fenêtre quand on clique
+
+    # Sidebar
     data = render_sidebar()
     if data == 0:  # Cas où aucun carburant n'est sélectionné
         st.warning("⚠️ la somme des carburants est nulle. Veuillez sélectionner au moins un carburant.")
         st.stop()
-        
+
+    # On affiche les propriétés du gaz
     show_gas_properties(data)
 
     try:
@@ -24,9 +43,11 @@ def main():
     except Exception as e:
         st.error(f"Erreur: {e}")
         st.stop()
-        
+
+    # On affiche les résultats
     show_limites_results(limites)
-    
+
+    # Analyse en profondeur
     render_analysis_expander()    
 
 
